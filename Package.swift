e9844834dbdd334ee7ebd0039f6ce708ec325ae1
Syed Faizan Ahmed <faizan.ahmed@purevpn.com>
2024-02-29 18:00:01 +0500

    Updated the dependency and linked it with Atom branch, as soon as the code will be merged in original repo it will be changed back to the original wire guard branch.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AtomWireguardTunnel",
     platforms: [
-        .iOS(.v12), .macOS(.v10_14)
+        .iOS(.v12), .macOS(.v10_14), .tvOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -15,7 +15,15 @@ let package = Package(
             targets: ["AtomWireguardTunnel"]),
     ],
     dependencies: [
-        .package(name: "WireGuardKit", url: "https://github.com/wireguard/wireguard-apple", branch: "am/develop")
+        
+        // tvOS from Atom branch, needs testing on macOS and iOS.
+        .package(name: "WireGuardKit", url: "https://github.com/AtomSDK/wireguard-apple", branch: "feature/tvos-compatibility")
+        
+        // Working fine on tvOS, needs testing on macOS and iOS.
+        //.package(name: "WireGuardKit", url: "https://github.com/passepartoutvpn/wireguard-apple", revision: "b79f0f150356d8200a64922ecf041dd020140aa0")
+        
+        // Old without tvOS, working fine on macOS and iOS.
+        //.package(name: "WireGuardKit", url: "https://github.com/wireguard/wireguard-apple", branch: "am/develop")
     ],
     targets: [
         .target(
