f5b080edc45d315eb560598f003588d16ee578ec
DevAtomAndroid <developer@atomapi.com>
2024-08-15 18:09:35 +0500

    - Change of tag.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         //.package(name: "WireGuardKit", url: "https://github.com/wireguard/wireguard-apple", branch: "am/develop")
-        .package(url: "https://github.com/AtomSDK/wireguard-apple", from: "1.0.18")
+        .package(url: "https://github.com/AtomSDK/wireguard-apple", from: "2.0.0")
     ],
     targets: [
         .target(
