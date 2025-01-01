7445ee9e4620c747bc4a1230e017e1a0407bbe44
DevAtomAndroid <developer@atomapi.com>
2025-01-01 20:13:35 +0500

    - Change of package version.

--- Package.swift
+++ Package.swift
@@ -18,9 +18,7 @@ let package = Package(
     ],
     dependencies: [
         //.package(name: "WireGuardKit", url: "https://github.com/wireguard/wireguard-apple", branch: "am/develop")
-//        .package(url: "https://github.com/AtomSDK/wireguard-apple", from: "3.0.0")
-//        .package(path: "../wireguard-apple")
-        .package(url: "https://github.com/AtomSDK/wireguard-apple", branch: "feature/pause-vpn")
+        .package(url: "https://github.com/AtomSDK/wireguard-apple", from: "3.1.0")
     ],
     targets: [
         .target(
